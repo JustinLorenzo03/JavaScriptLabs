c96caafa508dcc719ef5eace6a29a62d53d12d53
Justin Lorenzo <justin.lorenzo6699@gmail.com>
2020-11-11 14:36:12 -0500

    having the final lab done

--- prompt.js
+++ prompt.js
@@ -1,6 +1,12 @@
-const lowNumber = prompt('What is your low number?');
-const highNumber = prompt('What is your high Number?');
+// Asking the user for numbers
+const lowNumber = prompt('What is your low number');
+const highNumber = prompt('What is your high Number');
 
+//Convert the strings to numbers
+const LowNum = parseInt(lowNumber);
+const HighNum = parseInt(highNumber);
+
+//Having the calculated equations 
 const add = highNumber + lowNumber;
 const sub = highNumber - lowNumber;
 const divide = highNumber / lowNumber;
@@ -8,6 +14,29 @@ const multiply = highNumber * lowNumber;
 const expo = Math.pow(highNumber, lowNumber);
 const modulus = highNumber % lowNumber;
 
-const randomNumber = Math.floor( Math.random() * (highNumber - lowNumber + 1)  );
+//Pushing to the console for all the answers 
+console.log(add);
+console.log(sub);
+console.log(divide);
+console.log(multiply);
+console.log(expo);
+console.log(modulus);
 
+//Random number from 0-35, making sure not 0 and can be 35
+const randomNumber = Math.floor( Math.random() * 35 ) + 1 ;
 console.log(randomNumber);
+
+// float to the ceiling 
+
+const floated = prompt ('Enter any number with a decimal');
+const numCiel = parseInt(floated);
+const ceilNumber = Math.ceil (Math.random() * numCiel ) + 1;
+console.log(ceilNumber);
+
+
+// float to the floor
+
+const numFloor = prompt ('Enter any number with a decimal');
+const floutDown = parseInt(numFloor);
+const floatNumber = Math.floor (Math.random() * floutDown ) + 1;
+console.log(floatNumber);
