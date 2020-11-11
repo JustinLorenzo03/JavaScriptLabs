797031f046a0564969e9e1b359c89c457c17958c
Justin Lorenzo <justin.lorenzo6699@gmail.com>
2020-11-11 13:35:48 -0500

    worked on the number algorithms

--- prompt.js
+++ prompt.js
@@ -1,6 +1,13 @@
 const lowNumber = prompt('What is your low number?');
 const highNumber = prompt('What is your high Number?');
 
+const add = highNumber + lowNumber;
+const sub = highNumber - lowNumber;
+const divide = highNumber / lowNumber;
+const multiply = highNumber * lowNumber;
+const expo = Math.pow(highNumber, lowNumber);
+const modulus = highNumber % lowNumber;
+
 const randomNumber = Math.floor( Math.random() * (highNumber - lowNumber + 1)  );
 
 console.log(randomNumber);
